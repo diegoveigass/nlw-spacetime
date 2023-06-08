a1e0e9c2669bbebe6f11fa413163f0ccf0c1093e
Diego Veiga <diego.veiga@xmobots.com.br>
2023-06-08 09:45:57 -0300

    finish third class mobile

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react'
 import { StatusBar } from 'expo-status-bar'
+import { useRouter } from 'expo-router'
 import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
 import {
   useFonts,
@@ -7,19 +9,62 @@ import {
 } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 import { styled } from 'nativewind'
+import * as SecureStore from 'expo-secure-store'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import { api } from '../src/lib/api'
 
-import blugBg from './src/assets/bg-blur.png'
-import Stripes from './src/assets/stripes.svg'
-import NlwSpacetimeLogo from './src/assets/nlw-spacetime-logo.svg'
+import blugBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import NlwSpacetimeLogo from '../src/assets/nlw-spacetime-logo.svg'
 
 const StyledStripes = styled(Stripes)
 
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/f65ec44ee25bc82129ad',
+}
+
 export default function App() {
+  const router = useRouter()
+
   const [hasLoadedFonts] = useFonts({
     BaiJamjuree_700Bold,
     Roboto_400Regular,
     Roboto_700Bold,
   })
+
+  const [request, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: 'f65ec44ee25bc82129ad',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  )
+
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+
+    const { token } = response.data
+
+    await SecureStore.setItemAsync('token', token)
+
+    router.push('/memories')
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params
+
+      handleGithubOAuthCode(code)
+    }
+  }, [response])
 
   if (!hasLoadedFonts) {
     return null
@@ -49,6 +94,7 @@ export default function App() {
         <TouchableOpacity
           className="rounded-full bg-green-500 px-5 py-2"
           activeOpacity={0.7}
+          onPress={() => signInWithGithub()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             Cadastrar lembrança
--- server/src/server.ts
+++ server/src/server.ts
@@ -22,6 +22,7 @@ app.register(memoriesRoutes)
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('👑 listening on  http://localhost:3333')
